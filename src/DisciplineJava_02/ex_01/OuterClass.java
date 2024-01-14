323826c2ea87c2fa4fea1d68102ba109cce33f2e
r1605866 <r1605866@naver.com>
2024-01-14 14:12:47 +0900

    [2024-01-14]인스턴스 내부 클래스 과제 업로드

--- src/DisciplineJava_02/ex_01/OuterClass.java
+++ src/DisciplineJava_02/ex_01/OuterClass.java
@@ -1,14 +1,20 @@
 package DisciplineJava_02.ex_01;
 
 // Q1.인스턴스 내부 클래스를 정의하고, 사용해보세요.
-public class OuterClass { // OuterClass
+public class OuterClass { // 외부 클래스
     private int outVal = 100;
-    public class InnerClass{
+
+    public void printValsOuter(){
+        System.out.println("보근 강사 짱짱맨 ~ !");
+    }
+    public class InnerClass{ // 내부 클래스
         private int inVal = 200;
 
-        public void printVals(){
+        public void printValsInner(){
             System.out.println("outVal = " + outVal);
             System.out.println("inVal = " + inVal);
+
+            printValsOuter();
         }
     }
 }
