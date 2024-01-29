66f04c7f90f33521c454e7daa0180d97a8f255cb
r1605866 <r1605866@naver.com>
2024-01-29 21:01:53 +0900

    [2024-01-29]자바의 상속 코드로 짜보기(내용추가)

--- src/DisciplineJava_04/ex01/ex01.java
+++ src/DisciplineJava_04/ex01/ex01.java
@@ -2,7 +2,7 @@ package DisciplineJava_04.ex01;
 
 class Parent{
     public String firstName;
-    public final String lastName = "Park";
+    public final String lastName = "GIL";
 
     public Parent(String firstName){
         this.firstName = firstName;
@@ -25,6 +25,7 @@ class Child extends Parent{
 
         System.out.println("첫번째 생성자 호출");
         System.out.println("내 이름은 " + super.firstName + "이고 나이는"+ age + "세 입니다.");
+        System.out.println(lastName + "내 성은 호출할 수 있지만 final 상수값이라 변경이 불가능하다.");
     }
 
     public Child(String firstName,int age){
@@ -34,6 +35,9 @@ class Child extends Parent{
         System.out.println("두번째 생성자 호출");
         System.out.println("내 이름은 " + firstName + "이고 나이는"+ age + "세 입니다.");
     }
+
+    // private로 설정된 sayMyName() 메서드는 자식인 Child한테 물려주지 않는다.
+
 }
 public class ex01 {
     public static void main(String[] args) {
